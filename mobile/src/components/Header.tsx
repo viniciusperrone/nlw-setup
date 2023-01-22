77febc1ae3b14520ec49fd082b7388887cbbf68c
Vinicius Perrone <perronevinicius2018@gmail.com>
2023-01-22 19:53:42 -0300

    feat: navigation and screens

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,10 +1,11 @@
 import { View, TouchableOpacity, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
-
+import { useNavigation } from "@react-navigation/native";
 import Logo from "../assets/logo.svg";
 
 export function Header() {
+  const { navigate } = useNavigation();
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -12,6 +13,7 @@ export function Header() {
       <TouchableOpacity
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
         activeOpacity={0.7}
+        onPress={() => navigate("new")}
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
         <Text className="text-white ml-3 font-semibold text-base">Novo</Text>
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -10,6 +11,7 @@ const minimumSummaryDateSize = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDateSize - dateFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -31,7 +33,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {dateFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, index) => {
